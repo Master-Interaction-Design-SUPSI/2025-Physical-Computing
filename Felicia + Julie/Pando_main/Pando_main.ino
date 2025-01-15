467f02633bd170c51ed1436d1db8510062d4b2d2
felicia-s-g <ghercavii.felicia@gmail.com>
2025-01-15 12:27:16 +0100

    Update Pando_main.ino

--- Felicia + Julie/Pando_main/Pando_main.ino
+++ Felicia + Julie/Pando_main/Pando_main.ino
@@ -3,7 +3,7 @@
 //library
 
 //Variable
-//button 
+//button
 bool btn_left_val = false;
 bool old_btn_left_val = false;
 int btn_left_pin = 11;
@@ -14,76 +14,29 @@ int count_left = 0;
 bool btn_right_val = false;
 bool old_btn_right_val = false;
 int btn_right_pin = 12;
-int btn_right_status = 0;  
+int btn_right_status = 0;
 int old_btn_right_status = -1;
 int count_right = 0;
 
 int randNumber;
 
-//LED 
-#include <Adafruit_NeoPixel.h>
-int led_status = false; 
-int pixels_count = 24;
-byte pixels_pin = 6; // its actually pin nr. 3 on the arudino board
-Adafruit_NeoPixel pixels(pixels_count, pixels_pin, NEO_GRB + NEO_KHZ800);
-
+int currentState = 0;
 
 void setup() {
-	Serial.begin(9600);
-
-  ///distance sensor
-  //while (! Serial) {
-  //  delay(1);
-  //}
-//
-  //Serial.println("Adafruit VL53L0X test.");
-  //if (!lox.begin(0x30)) {
-  //  Serial.println(F("Failed to boot VL53L0X"));
-  //  while(1);
-  //}
-  //// start continuous ranging
-  //lox.startRangeContinuous();
+  Serial.begin(9600);
 
   randomSeed(analogRead(0));
 
   //set up button
   pinMode(btn_left_pin, INPUT);
   pinMode(btn_right_pin, INPUT);
-
 }
 
 void loop() {
 
-  //ultra sonic distance sensor
-  //if (lox.isRangeComplete()) {
-  //  Serial.print("Distance in mm: ");
-  //  Serial.println(lox.readRange());
-  //}
-
-  //if (ultrasonic_distance >= 15 && ultrasonic_distance <= 20 && led_status == false){
-  //  Serial.println("I am activated");
-  //  lightNeutral();  //TODO: i need to make it light up gently 
-  //  led_status = true; 
-  //}
-  
-  bool btn_left = readButton(btn_left_pin, btn_left_val, old_btn_left_val, btn_left_status, old_btn_left_status);
-  bool btn_right = readButton(btn_right_pin, btn_right_val, old_btn_right_val, btn_right_status, old_btn_right_status);
 
 
-  if (btn_left_status = 2 && btn_left_status = 2){
-    lightShow();
 
-  }
-
-  if (btn_left_status = 2 ){
-    lightGreen();
-
-  }
-
-  if (btn_right_status = 2){
-    lightRed();
-
-  }
 
   // Selecting message to print
   getMessage(btn_left, btn_right, false);
@@ -91,41 +44,36 @@ void loop() {
   // Printing the message
 }
 
+void stateMachine() {
+  switch (currentState) {
+    case 0:
+      // if we detect motion go to 1
+      break;
+    case 1:  //we welcome the user
+      // we light up...
+      // we read buttons
+      bool btn_left = readButton(btn_left_pin, btn_left_val, old_btn_left_val, btn_left_status, old_btn_left_status);
+      bool btn_right = readButton(btn_right_pin, btn_right_val, old_btn_right_val, btn_right_status, old_btn_right_status);
 
-void eyeClose(){
-  servo.write(0);
-  delay(500);
-}
+      if (btnLeft && btnRight) {
+        currentState = 2;
+      }
+      if (btnLeft& != btnRight) {
+        currentState = 3;
 
-void eyeOpen(){
-  servo.write(90);
-  delay(500);
-}
+        randNumber = random(0, sizeof(btnLeft) / sizeof(btnLeft[0]));
+        Serial.println(btnLeft[randNumber]);
+      }
+      if (btnRight) {
+        randNumber = random(0, sizeof(btnRight) / sizeof(btnRight[0]));
+        Serial.println(btnRight[randNumber]);
+      }
+      case 2:  //BIG REWARD
+        randNumber = random(0, sizeof(btnBoth) / sizeof(btnBoth[0]));
+        Serial.println(btnBoth[randNumber]);
+        currentState = 0;
 
-void lightNeutral(){
-  pixels.clear();
 
-   for(int i=0; i<pixels_count; i++) {
-      pixels.setPixelColor(i, pixels.Color(255, 255, 255));
+      break;
   }
-  pixels.show();
 }
-
-void lightRed(){
-  pixels.clear();
-
-   for(int i=0; i<pixels_count; i++) {
-      pixels.setPixelColor(i, pixels.Color(255, 0, 0));
-  }
-  pixels.show();
-}
-
-void lightGreen(){
-  pixels.clear();
-
-   for(int i=0; i<pixels_count; i++) {
-      pixels.setPixelColor(i, pixels.ColorHSV(0, 255, 0));
-  }
-  pixels.show();
-}
-
